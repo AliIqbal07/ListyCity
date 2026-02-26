da1e978b88e64d9d7e0220c14bdc0fff2c4f65b4
AliIqbal07 <iqbal.ali0304@gmail.com>
2026-02-26 15:53:27 +0500

    lab_05 changes

--- app/src/main/java/com/example/listycity/MainActivity.java
+++ app/src/main/java/com/example/listycity/MainActivity.java
@@ -3,7 +3,8 @@ package com.example.listycity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.CollectionReference;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -12,23 +13,48 @@ import androidx.core.view.WindowInsetsCompat;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     ListView cityList;
     ArrayAdapter<String> cityAdapter;
     ArrayList<String> dataList;
+    FirebaseFirestore db;
+    CollectionReference citiesRef;
+    ArrayList<String> docIdList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        db = FirebaseFirestore.getInstance();
+        citiesRef = db.collection("cities");
+        dataList = new ArrayList<>();
+        docIdList = new ArrayList<>();
+        citiesRef.addSnapshotListener((value, error) -> {
+            if (error != null || value == null) return;
+
+            dataList.clear();
+            docIdList.clear();
+
+            for (var document : value) {
+                String cityName = document.getString("name");
+                String docId = document.getId();
+
+                dataList.add(cityName);
+                docIdList.add(docId);
+            }
+
+            cityAdapter.notifyDataSetChanged();
+        });
         cityList = findViewById(R.id.city_list);
         android.widget.EditText addCityEdit = findViewById(R.id.add_city_field);
         android.widget.Button addButton = findViewById(R.id.add_button);
         android.widget.Button deleteButton = findViewById(R.id.delete_button);
-        String []cities = {"Edmonton", "Vancouver", "Moscow", "Sydney", "Berlin", "Vienna", "Tokyo", "Beijing", "Osaka", "New Delhi"};
+
         dataList = new ArrayList<>();
-        dataList.addAll(Arrays.asList(cities));
+
         cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
         cityList.setAdapter(cityAdapter);
         final int[] selectedPosition = {-1};
@@ -38,15 +64,18 @@ public class MainActivity extends AppCompatActivity {
         addButton.setOnClickListener(v -> {
             String newCity = addCityEdit.getText().toString();
             if (!newCity.isEmpty()) {
-                dataList.add(newCity);
-                cityAdapter.notifyDataSetChanged();
+//                dataList.add(newCity);
+//                cityAdapter.notifyDataSetChanged();
+                Map<String, Object> city = new HashMap<>();
+                city.put("name", newCity);
+                citiesRef.add(city);
                 addCityEdit.setText("");
             }
         });
         deleteButton.setOnClickListener(v -> {
             if (selectedPosition[0] != -1) {
-                dataList.remove(selectedPosition[0]);
-                cityAdapter.notifyDataSetChanged();
+                String docId = docIdList.get(selectedPosition[0]);
+                citiesRef.document(docId).delete();
                 selectedPosition[0] = -1;
             }
         });
